6991acb95de5a3c87472bec90f4cbdd78d11dc82
mgemoraw <meng.get4@gmail.com>
2025-08-11 16:52:44 +0300

    updats

--- cmi/core/models.py
+++ cmi/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Tipper(models.Model):
+    license_plate = models.CharField(max_length=20, unique=True)
+    def __str__(self):
+        return self.license_plate
+
+class CollectorModel(models.Model):
+    fname = models.CharField(max_length=100)
+    mname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    phone = models.CharField(unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    
+class TipperModel(models.Model):
+    collector = models.ForeignKey('CollectorModel', on_delete=models.CASCADE)
+    tipper = models.ForeignKey('Tipper', on_delete=models.CASCADE)
--- cmi/core/views.py
+++ cmi/core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def dashboard(request):
     return render(request, 'core/dashboard.html', {})
+
+def trucks(request):
+    return render(request, 'core/truck.html', {})
+
+def truck_create(request):
+    if request.method == 'POST':
+        # Get form data
+        truck_id = request.POST.get('truck_id')
+        productivity = request.POST.get('productivity')
+
+        # Save to DB, handle validation, etc.
+        # Truck.objects.create(...)
+
+        return redirect('core:truck')  # or render with success msg
